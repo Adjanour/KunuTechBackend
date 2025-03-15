17e491bf71dbc5f44b1838008c42490d86e32d5c
adjanour <adjanour@icloud.com>
2025-03-15 02:08:36 +0000

    three

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,18 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
+import base64
 
-# Load Firebase Credentials
-cred = credentials.Certificate("app/core/kunutech-34a30-firebase-adminsdk-fbsvc-abcb1021e6.json")
+# Load the environment variable
+encoded_credentials = os.getenv("FIREBASE_CREDENTIALS")
+
+# Decode it
+firebase_credentials = json.loads(base64.b64decode(encoded_credentials))
+
+# Initialize Firebase Admin
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
+
 # Firestore Database
 db = firestore.client()
